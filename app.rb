e3eadf95fa8853414729f5461891b36ba451ce0d
Michael Brown and Robert Lee <robert@>
2018-12-03 12:48:34 -0800

    Update input view

--- app.rb
+++ app.rb
@@ -1,14 +1,38 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/Contact')
+require('./lib/list')
+require('./lib/task')
 require('pry')
+require('pg')
 
 DB = PG.connect({:dbname => "to_do"})
 
-# get ('/') do
-#   @contact = ''
-#   Address::Contact.clear
-#   erb(:input)
-# end
-#
+get ('/') do
+  @lists = List.all
+  erb(:input)
+end
+
+post ('/') do
+  new_list_owner = params.fetch('new-list')
+  temp_list = List.new({:name => new_list_owner, :id => nil})
+  new_owner_tasks =
+  temp_list.save
+  @lists = List.all
+  erb(:input)
+end
+
+get ('/my_list/:list_id') do
+  list_id = params[:list_id].to_i
+  current_list = List.all[list_id-1]
+  @list = current_list
+  all_tasks = Task.all
+  display_tasks = []
+  all_tasks.each do |task|
+    if task.list_id == list_id
+      display_tasks.push(task)
+    end
+  end
+  @tasks = display_tasks
+  erb(:my_list)
+end
