3f6ce35b288d586d8e4605ebfd0f0a6145520cfb
Michael Brown and Quinn Miller <quinn@>
2018-12-05 11:16:08 -0800

    add individual task viewer page

--- app.rb
+++ app.rb
@@ -24,33 +24,31 @@ end
 
 get ('/my_list/:list_id') do
   list_id = params[:list_id].to_i
-  current_list = List.all[0]
-
-  List.all.each do |list|
-    if list.id == list_id
-      current_list = list
-    end
-  end
-
-  @list = current_list
+  @list = List.find(list_id)
   @tasks = Task.for_list_id(list_id)
   erb(:my_list)
 end
 
 post ('/my_list/:list_id') do
   list_id = params[:list_id].to_i
-  current_list = List.all[0]
-  List.all.each do |list|
-    if list.id == list_id
-      current_list = list
-    end
-  end
-  @list = current_list
+  @list = List.find(list_id)
   new_description = params.fetch('new-task')
   new_task = Task.new({:description => new_description, :list_id => list_id})
   new_task.save
   @tasks = Task.for_list_id(list_id)
   erb(:my_list)
+end
+
+get ('/my_list/task/:task_id') do
+  @task = Task.find(params[:task_id])
+  erb(:task)
+end
+
+patch ('/my_list/task/:task_id') do
+  description = params.fetch("description")
+  @task = Task.find(params[:task_id].to_i)
+  @task.update({:description => description})
+  erb(:task)
 end
 
 delete ('/my_list/:list_id/:task_id') do
